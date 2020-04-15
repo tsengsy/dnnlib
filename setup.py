ed60f6c373b4e69a90de33d526ebfb38973334c9
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-04-15 09:53:09 -0400

    Deleted not dnnlib files. Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='dnnlib',
+
+    version='0.0.1',
+
+    description='dnnlib - from StyleGAN repository',
+
+    url='https://github.com/podgorskiy/dlutils',
+
+    author='Stanislav Pidhorskyi',
+    author_email='stanislav@podgorskiy.com',
+
+    license='Creative Commons Attribution-NonCommercial 4.0',
+
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    packages=['dnnlib'],
+)
